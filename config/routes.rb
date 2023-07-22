9b82caaf01e91ee0856379b5ee130ddd068e81db
sakai <eiichiro838@gmail.com>
2023-07-22 17:13:46 +0900

    コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
-    root "articles#index"
+  root "articles#index"
   
-    resources :articles
+  resources :articles do
+    resources :comments
   end
-  
+
 end
